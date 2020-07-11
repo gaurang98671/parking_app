5fd38c0c733fcd1a7d91e316bfd6b7c40a9f7eb0
VaibhavSingh29 <contacttovaibhav@gmail.com>
2020-07-11 12:59:40 +0530

    user profile and about

--- lib/user_profile.dart
+++ lib/user_profile.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,19 +6,19 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:path/path.dart';
 
 class user_profile extends StatefulWidget {
- 
   @override
   _user_profileState createState() => _user_profileState();
 }
 
 class _user_profileState extends State<user_profile> {
-  String current_users_email='';
+  String current_users_email = '';
 
-
-  Future getUserInfo() async
-  {
+  Future getUserInfo() async {
     var firestore = Firestore.instance;
-    QuerySnapshot qn = await firestore.collection("Users").where('email',isEqualTo: current_users_email).getDocuments();
+    QuerySnapshot qn = await firestore
+        .collection("Users")
+        .where('email', isEqualTo: current_users_email)
+        .getDocuments();
     return qn.documents;
   }
 
@@ -29,43 +27,73 @@ class _user_profileState extends State<user_profile> {
     // TODO: implement initState
     get_email();
     super.initState();
-
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-        future:  getUserInfo(),
-        builder: (_, snapshot)
-        {
-          if(!snapshot.hasData)
-            {
-              return Text("No profile");
-            }
-          else
-            {
-              return Container(
-                child: Column(
-                  children: <Widget>[
-                    get_profile(snapshot.data[0]["profile pic"]),
-                    GestureDetector(
-                      child: Text('Upload Picture',
-                      style: TextStyle(
-                        color: Colors.blue
+        // backgroundColor: Color(0xff020061),
+        body: FutureBuilder(
+      future: getUserInfo(),
+      builder: (_, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+              child: CircularProgressIndicator(
+            // valueColor: null,
+            backgroundColor: Color(0xffec8b5e),
+          ));
+        } else {
+          return Container(
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 80,
+                  ),
+                  GestureDetector(
+                      child: get_profile(snapshot.data[0]["profile pic"]),
+                      onTap: () {}),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Center(
+                        child: Text(
+                          snapshot.data[0]["Name"],
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
                       ),
+                      get_verify_tag(snapshot.data[0]['Verified']),
+                    ],
+                  ),
+                  ListTile(
+                      title: Center(child: Text('Phone')),
+                      subtitle: Center(
+                          child: Text(snapshot.data[0]["Phone Number"]))),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Card(
+                    child: ListTile(
+                      title: Text(
+                        'Vehicle registration plate',
+                        style: TextStyle(color: Colors.black87),
                       ),
-                      onTap: (){},
+                      subtitle: TextField(
+                        // onChanged: () => {},
+                        style: TextStyle(fontSize: 14, color: Colors.black45),
+                        decoration: null,
+                        maxLines: 1,
+                      ),
                     ),
-                    Text(snapshot.data[0]["Name"]),
-                    get_verify_tag(snapshot.data[0]['Verified']),
-                    Text(snapshot.data[0]["Phone Number"])
-                  ],
-                ),
-              );
-            }
-        },
-      )
-    );
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+      },
+    ));
   }
 
   /*void get_uid() async{
@@ -76,36 +104,35 @@ class _user_profileState extends State<user_profile> {
     });
   }*/
 
-  Widget get_profile(String profile_url)
-  {
-    if(profile_url=='none')
-      {
-        return Icon(Icons.account_circle, size: 50);
-      }
+  Widget get_profile(String profile_url) {
+    if (profile_url == 'none') {
+      return Icon(Icons.account_circle, size: 120);
+    }
     //else it will return circular avatar with user profile pic
-
   }
 
   Widget get_verify_tag(data) {
-
-    if(data==true)
-      {
-        return Icon(Icons.verified_user);
-      }
-    else
-      {
-        return Text(
-          'Not verified'
-        );
-      }
+    if (data == true) {
+      return Icon(
+        Icons.verified_user,
+        size: 13,
+        color: Colors.green,
+      );
+    } else {
+      return Text(
+        'Not verified',
+        style: TextStyle(
+          color: Colors.black45,
+          fontSize: 13,
+        ),
+      );
+    }
   }
 
-    Future<void> get_email() async{
-    SharedPreferences prefs= await SharedPreferences.getInstance();
+  Future<void> get_email() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
-      current_users_email=  prefs.getString('user_email');
+      current_users_email = prefs.getString('user_email');
     });
   }
-
-
 }
